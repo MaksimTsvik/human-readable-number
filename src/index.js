754bde7172fe4c96098d1e306c2c4598d22275ea
MaksimTsvik <mkdemix@tut.by>
2021-01-31 23:06:23 +0300

    Updated solution.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ module.exports = function toReadable(number) {
         'eighteen', 'nineteen'];
     let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty',
         'ninety'];
+    let numberArr = number.toString().split('');
 
     if (number < 20) {
         return ones[+number];
+    }
+
+    if (number < 100 && number > 19) {
+        return (tens[numberArr[0]] + ' ' + ones[numberArr[1]]);
+    }
+
+    if (number > 99) {
+        return (ones[numberArr[0]] + ' hundred ' + tens[numberArr[1]] + ' ' + ones[numberArr[2]]);
     }
 
 }
